e0d6fa78f2fac6d90aed1eb520562a11348badea
Dulmandakh <dulmandakh@gmail.com>
2021-10-17 11:44:44 +0800

    test allow_basic_auth (#8)

--- ariadne_django_ext/utils.py
+++ ariadne_django_ext/utils.py
@@ -2,7 +2,7 @@ from django.core.exceptions import PermissionDenied
 
 
 def is_authenticated(request, is_active=True, raise_exception=True):
-    user = getattr(request, "user")
+    user = getattr(request, "user", None)
     if user and user.is_authenticated and (not is_active or user.is_active):
         return user
     if raise_exception:
